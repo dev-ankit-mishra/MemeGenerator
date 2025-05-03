84823470a75c8e7e9c14a20b7854d1e43e94cc00
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-05-03 23:15:11 +0530

    Added input Box

---
+++ MemeGenerator/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <main>
+            <header>
+                <img src="" alt="meme-icon"/>
+                <h2>Meme Generator</h2>
+            </header>
+            <section className="input">
+                <label>
+                    Top Text
+                    <br/>
+                    <input type="text"/>
+                </label>
+                <label>
+                    Bottom Text
+                    <br/>
+                    <input type="text"/>
+                </label>
+
+            </section>
+            <button>
+                Get a new meme image
+            </button>
+        </main>
+    )
+}
